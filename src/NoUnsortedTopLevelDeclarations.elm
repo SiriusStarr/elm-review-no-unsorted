4aa59f627889201c1566e5ea282c82983512ad60
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2022-01-05 13:14:04 -0800

    🐛 Fix doc comments not moving with ports

--- src/NoUnsortedTopLevelDeclarations.elm
+++ src/NoUnsortedTopLevelDeclarations.elm
@@ -43,10 +43,12 @@ glued before `b` and `b` is glued after `c`, then the result will be `c` -> `a`
 
 import Elm.Syntax.Declaration exposing (Declaration(..))
 import Elm.Syntax.Exposing as Exposing exposing (TopLevelExpose(..))
+import Elm.Syntax.Import exposing (Import)
 import Elm.Syntax.Module as Module exposing (Module)
 import Elm.Syntax.Node as Node exposing (Node)
 import Elm.Syntax.Range as Range exposing (Range)
 import List.Extra as ListX
+import Maybe.Extra as MaybeX
 import Review.Rule as Rule exposing (Error, Rule)
 import Set exposing (Set)
 import String.Extra as StringX
@@ -153,6 +155,8 @@ rule : RuleConfig r -> Rule
 rule (RuleConfig r) =
     Rule.newModuleRuleSchemaUsingContextCreator "NoUnsortedTopLevelDeclarations" initialContext
         |> Rule.withModuleDefinitionVisitor (\m c -> ( [], getModuleExports m c ))
+        |> Rule.withCommentsVisitor (\cs context -> ( [], getUnparsedDocComments cs context ))
+        |> Rule.withImportVisitor (\i context -> ( [], accumulateImportRange i context ))
         |> Rule.withDeclarationListVisitor
             (\ds c ->
                 ( declarationVisitor
@@ -186,6 +190,8 @@ type alias Context =
     { extractSource : Range -> String
     , exports : Maybe (List String)
     , errorRange : Range
+    , unparsedDocComments : List (Node String)
+    , moduleImportRange : Maybe Range
     }
 
 
@@ -782,6 +788,8 @@ initialContext =
             { extractSource = extractSource
             , exports = Nothing
             , errorRange = Range.emptyRange
+            , unparsedDocComments = []
+            , moduleImportRange = Nothing
             }
         )
         |> Rule.withSourceCodeExtractor
@@ -836,6 +844,36 @@ getModuleExports m context =
             }
 
 
+{-| Get an ordered list of all doc comments in the module that were not parsed
+by `elm-syntax`.
+-}
+getUnparsedDocComments : List (Node String) -> Context -> Context
+getUnparsedDocComments comments context =
+    { context
+        | unparsedDocComments =
+            List.filter
+                (Node.value
+                    >> (\c ->
+                            String.startsWith "{-|" c
+                                && String.endsWith "-}" c
+                       )
+                )
+                comments
+    }
+
+
+{-| Collect the range of imports in the module.
+-}
+accumulateImportRange : Node Import -> Context -> Context
+accumulateImportRange i context =
+    { context
+        | moduleImportRange =
+            MaybeX.unpack (\() -> Just <| Node.range i)
+                (\r -> Just <| Range.combine [ r, Node.range i ])
+                context.moduleImportRange
+    }
+
+
 {-| Generate declaration info for all TLDs and then check that they are sorted.
 -}
 declarationVisitor : RuleConfig r -> List (Node Declaration) -> Context -> List (Error {})
@@ -845,16 +883,58 @@ declarationVisitor (RuleConfig { glues, sortBy }) decs context =
         applyGlues ds i d =
             { d | glued = ListX.findMap (\g -> g ( i, d ) ds) glues }
     in
-    List.filterMap (getDecInfo context.exports) decs
+    ListX.uncons context.unparsedDocComments
+        |> MaybeX.unwrap []
+            (\( c, cs ) ->
+                case Maybe.map (Range.compare (Node.range c)) context.moduleImportRange of
+                    Just GT ->
+                        -- First doc comment is after imports, so cannot be module-associated.
+                        context.unparsedDocComments
+
+                    Nothing ->
+                        -- No module imports, so have to figure it out manually
+                        -- If there are two doc comments before first TLD, then
+                        -- the first must be module But do not need to test for
+                        -- this, because the first would get dropped anyways,
+                        -- since only the latter would possibly get attached.
+                        -- Similarly, do not need to worry if the first TLD has
+                        -- a parsed doc comment, because it would get dropped by
+                        -- going past that TLD. If first is a doc comment with a
+                        -- line that begins with "@docs", then it is essentially
+                        -- guaranteed to be for the module, however.
+                        if List.any (String.startsWith "@docs") <| String.lines <| Node.value c then
+                            cs
+
+                        else
+                            -- There is no @docs in the comment, so this might be a doc comment belonging to the first TLD, if it is a port.
+                            context.unparsedDocComments
+
+                    _ ->
+                        -- First doc comment is before imports, so is the module doc comment, so drop it
+                        cs
+            )
+        |> (\docCommentsWithoutModule ->
+                List.foldl (getDecInfo context.exports) ( [], docCommentsWithoutModule ) decs
+           )
+        |> Tuple.first
+        |> List.reverse
         |> (\ds -> List.indexedMap (applyGlues ds) ds)
         |> checkSortingWithGlue context.extractSource "Top-level declarations" sortBy context.errorRange
 
 
 {-| Given a list of module exports, generate TLD info from a `declaration`.
 -}
-getDecInfo : Maybe (List String) -> Node Declaration -> Maybe TLD
-getDecInfo exports d =
-    case Node.value d of
+getDecInfo : Maybe (List String) -> Node Declaration -> ( List TLD, List (Node String) ) -> ( List TLD, List (Node String) )
+getDecInfo exports d ( acc, unparsedDocComments ) =
+    let
+        ( immediatelyPreviousDocComment, remainingDocComments ) =
+            ListX.splitWhen (\c -> Range.compare (Node.range c) (Node.range d) == GT) unparsedDocComments
+                |> Maybe.withDefault
+                    -- No doc comments were after the TLD, so it must be the last
+                    ( unparsedDocComments, [] )
+                |> Tuple.mapFirst ListX.last
+    in
+    ( case Node.value d of
         FunctionDeclaration { declaration } ->
             Node.value declaration
                 |> (\{ name, expression } ->
@@ -892,7 +972,9 @@ getDecInfo exports d =
             Just
                 { type_ = Port
                 , namesBound = Set.singleton <| Node.value name
-                , range = Node.range d
+
+                -- Glue doc comment on, since port doc comments are not yet parsed by `elm-syntax`
+                , range = MaybeX.unwrap (Node.range d) (\c -> Range.combine [ Node.range c, Node.range d ]) immediatelyPreviousDocComment
                 , glued = Nothing
 
                 -- Ports can't be exposed or dependent on other TLDs
@@ -905,3 +987,6 @@ getDecInfo exports d =
             -- Destructuring (Node Pattern) (Node Expression)
             -- InfixDeclaration Infix
             Nothing
+    , remainingDocComments
+    )
+        |> Tuple.mapFirst (MaybeX.unwrap acc (\tld -> tld :: acc))
